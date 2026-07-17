138998436d87714382b3ee21f071c2d32397526f
Pranjal Gupta <pranjal.gupta.1586@gmail.com>
2026-07-17 14:20:14 +0530

    creation of ex1.js

---
+++ ex1.js
@@ -0,0 +1,11 @@
+// let a=10;
+// const b=20;
+// console.log("a=",a);
+// console.log("b=",b);
+// b=a++;
+// console.log("b=",b);
+
+const sum=(a,b)=>a+b;
+console.log("Sum:", sum(5, 10));
+console.log("Sum:", sum(15, 25));
+console.log("Sum:", sum(100, 200));
